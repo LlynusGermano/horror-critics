9b6c25b941ace0257a1ebab21c3f2fd29b7bdc99
LlynusGermano <134547384+LlynusGermano@users.noreply.github.com>
2023-06-09 15:18:14 -0300

    commit d
    alpha teste 04

---
+++ script.js
@@ -0,0 +1,134 @@
+
+
+function panico() {
+    var containerpanico = document.getElementById("selec_panico");
+    var containersexta = document.getElementById("selec_sexta");
+    var containerinvocacao = document.getElementById("selec_invocacao");
+    var containerhalloween = document.getElementById("selec_halloween");
+    var containerpesadelo = document.getElementById("selec_pesadelo");
+    if (containersexta.style.display === "flex") {
+        containersexta.style.display = "none"
+    }
+    if (containerinvocacao.style.display === "flex") {
+        containerinvocacao.style.display = "none"
+    }
+    if (containerhalloween.style.display === "flex") {
+        containerhalloween.style.display = "none"
+    }
+    if (containerpesadelo.style.display === "flex") {
+        containerpesadelo.style.display = "none"
+    }
+    if (containerpanico.style.display === "flex") {
+        containerpanico.style.display = "none"
+    } else {
+        containerpanico.style.display = "flex"
+    }
+};
+
+function halloween() {
+    var containerpanico = document.getElementById("selec_panico");
+    var containersexta = document.getElementById("selec_sexta");
+    var containerinvocacao = document.getElementById("selec_invocacao");
+    var containerhalloween = document.getElementById("selec_halloween");
+    var containerpesadelo = document.getElementById("selec_pesadelo");
+
+    if (containersexta.style.display === "flex") {
+        containersexta.style.display = "none"
+    }
+    if (containerinvocacao.style.display === "flex") {
+        containerinvocacao.style.display = "none"
+    }
+    if (containerpesadelo.style.display === "flex") {
+        containerpesadelo.style.display = "none"
+    }
+    if (containerpanico.style.display === "flex") {
+        containerpanico.style.display = "none"
+    }
+
+    if (containerhalloween.style.display === "flex") {
+        containerhalloween.style.display = "none"
+    } else {
+        containerhalloween.style.display = "flex"
+    }
+};
+
+function sexta() {
+    var containerpanico = document.getElementById("selec_panico");
+    var containersexta = document.getElementById("selec_sexta");
+    var containerinvocacao = document.getElementById("selec_invocacao");
+    var containerhalloween = document.getElementById("selec_halloween");
+    var containerpesadelo = document.getElementById("selec_pesadelo");
+
+    if (containerpanico.style.display === "flex") {
+        containerpanico.style.display = "none"
+    }
+    if (containerpesadelo.style.display === "flex") {
+        containerpesadelo.style.display = "none"
+    }
+    if (containerinvocacao.style.display === "flex") {
+        containerinvocacao.style.display = "none"
+    }
+    if (containerhalloween.style.display === "flex") {
+        containerhalloween.style.display = "none"
+    }
+
+    if (containersexta.style.display === "flex") {
+        containersexta.style.display = "none"
+    } else {
+        containersexta.style.display = "flex"
+    }
+};
+
+function pesadelo() {
+    var containerpanico = document.getElementById("selec_panico");
+    var containersexta = document.getElementById("selec_sexta");
+    var containerinvocacao = document.getElementById("selec_invocacao");
+    var containerhalloween = document.getElementById("selec_halloween");
+    var containerpesadelo = document.getElementById("selec_pesadelo");
+
+    if (containerpanico.style.display === "flex") {
+        containerpanico.style.display = "none"
+    }
+    if (containersexta.style.display === "flex") {
+        containersexta.style.display = "none"
+    }
+    if (containerinvocacao.style.display === "flex") {
+        containerinvocacao.style.display = "none"
+    }
+    if (containerhalloween.style.display === "flex") {
+        containerhalloween.style.display = "none"
+    }
+
+    if (containerpesadelo.style.display === "flex") {
+        containerpesadelo.style.display = "none"
+    } else {
+        containerpesadelo.style.display = "flex"
+    }
+};
+
+function invocacao() {
+    var containerpanico = document.getElementById("selec_panico");
+    var containersexta = document.getElementById("selec_sexta");
+    var containerinvocacao = document.getElementById("selec_invocacao");
+    var containerhalloween = document.getElementById("selec_halloween");
+    var containerpesadelo = document.getElementById("selec_pesadelo");
+
+    if (containerpanico.style.display === "flex") {
+        containerpanico.style.display = "none"
+    }
+    if (containersexta.style.display === "flex") {
+        containersexta.style.display = "none"
+    }
+    if (containerhalloween.style.display === "flex") {
+        containerhalloween.style.display = "none"
+    }
+    if (containerpesadelo.style.display === "flex") {
+        containerpesadelo.style.display = "none"
+    }
+
+    if (containerinvocacao.style.display === "flex") {
+        containerinvocacao.style.display = "none"
+    } else {
+        containerinvocacao.style.display = "flex"
+    }
+};
